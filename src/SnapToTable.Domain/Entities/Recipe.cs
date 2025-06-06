c8902555b2a1faa8db98f32419782c7561f0fe19
PL <pilouk@gmail.com>
2025-06-06 19:31:26 -0400

    Add entities

---
+++ src/SnapToTable.Domain/Entities/Recipe.cs
@@ -0,0 +1,13 @@
+﻿namespace SnapToTable.Domain.Entities;
+
+public record Recipe(
+    DateTime CreatedAt,
+    Guid Id,
+    Guid RecipeAnalysisRequestId,
+    string Title,
+    string Description,
+    IReadOnlyList<string> Ingredients,
+    string Instructions,
+    int PrepTimeMinutes,
+    int CookTimeMinutes
+);
---
+++ src/SnapToTable.Domain/Entities/RecipeAnalysisRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SnapToTable.Domain.Entities;
+
+public record RecipeAnalysisRequest(
+    DateTime CreateDateTime,
+    Guid Id,
+    string Name,
+    IReadOnlyList<string> ImageUrls
+);
